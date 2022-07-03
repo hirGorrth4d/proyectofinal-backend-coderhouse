1f5c5a5e536cd8cb4733f1201670a6b380df660e
Martin Cagliolo <36281151+sonovdragons@users.noreply.github.com>
2022-07-02 21:37:06 -0300

    first commit

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,22 +1,14 @@
-const fs =require("fs")
 const {Carrito} = require('../classes/carrito')
 const {Producto} = require('../classes/producto')
 const carrito = new Carrito(__dirname + "/data/carrito.json")
 const producto = new Producto(__dirname + "/data/productos.json")
 
 const getCarrito = (req, res) => {
-    const cart = carrito.getAll()
-    res.json(cart)
+    
 }
 
 const addCarrito = (req,res) =>{
-    const products = req.body.products.map(Number)
-    const allProducts = producto.getAll()
-    const productFind = allProducts.flter((product) => {
-        products.includes(product.id)
-    })
-    const cart = carrito.save({products: productFind})
-    res.json(cart)
+    
 }
 
 module.exports = {
